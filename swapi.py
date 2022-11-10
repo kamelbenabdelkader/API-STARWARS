17c4fd45a5386f0b6da119f9e5a82eed8d9eda78
kamelbenabdelkader <41142960+kamelbenabdelkader@users.noreply.github.com>
2022-11-10 12:14:42 +0100

    Update swapi.py

--- swapi.py
+++ swapi.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import requests
+
+
+element = ''
+
+def recup(element):
+    url = 'https://swapi.dev/api/'+'?page='{1}
+    reponse = requests.get(url+element)
+    if reponse.status_code == 200:
+        data = reponse.json()
+        dataok = pd.DataFrame(data['results'])
+    else:
+        print("votre requete avec votre api n'a pas aboutit")
+    return dataok
 
