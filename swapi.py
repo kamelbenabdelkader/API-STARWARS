69598cb1221acf7985e0cd2f60399e19ac5d1441
lauradesjacques <desjacques.lauralee@gmail.com>
2022-11-10 13:37:30 +0100

    creation fonction recovery-url

--- swapi.py
+++ swapi.py
@@ -1,2 +1,13 @@
 import pandas as pd
 import requests
+
+# On crée une fonction qui va appeler l'api.
+def recovery_url(categorie:str):
+    https = "https://swapi.dev/api/"
+    url = https + categorie
+    if requests.get(url):
+        reponse = requests.get(url)
+        print("===> la requête s’est déroulée correctement")
+    else:
+        print("ERREUR : la requête s’est pas déroulée correctement")
+    return reponse
